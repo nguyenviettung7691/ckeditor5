9bbd0b6e7e8eebd7d78cb01f7d46f533362314ae
Harry <tung.nguyen@orientsoftware.net>
2020-07-31 14:25:25 +0700

    Ckeditor  - Add plugin Underline, Subscript, Superscript

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -12,6 +12,9 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -41,8 +44,7 @@ const plugins = [
 	Essentials,
 	UploadAdapter,
 	Autoformat,
-	Bold,
-	Italic,
+	Bold, Italic,Underline, Subscript, Superscript,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -71,8 +73,7 @@ const config = {
 		items: [
 			'heading',
 			'|',
-			'bold',
-			'italic',
+			'bold', 'italic', 'underline', 'subscript', 'superscript',
 			'link',
 			'bulletedList',
 			'numberedList',
